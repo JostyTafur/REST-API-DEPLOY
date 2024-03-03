9bf0a4b300d29ddb4bda6cd2cb2894ebae2bcaf7
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2024-03-03 10:23:48 -0500

    Deploy API to FL0

---
+++ schemas/movies.js
@@ -0,0 +1,36 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Title must be a string',
+    required_error: 'Title is required'
+  }),
+  year: z.number().int().positive().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(0),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['action', 'comedy', 'drama', 'horror', 'romance', 'sci-fi']),
+    {
+      required_error: 'Genre is required',
+      invalid_type_error: 'Genre must be an array of strings',
+      invalid_enum_error: 'Invalid genre'
+    }
+  )
+})
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
